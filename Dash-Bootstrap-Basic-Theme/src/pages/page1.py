0f110aeaca789f3cca4740c9e03881e56f7d8d68
Akash Kaul <akashkaul@Akashs-MacBook-2032.local>
2020-08-26 17:59:26 -0500

    added plotly figures

--- Dash-Bootstrap-Basic-Theme/src/pages/page1.py
+++ Dash-Bootstrap-Basic-Theme/src/pages/page1.py
@@ -2,30 +2,39 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_cytoscape as cyto
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import datetime
 import numpy as np
+import plotly.express as px
 
-#START-HEATMAP-COMPONENT
-# np.random.seed(1)
+# START-HEATMAP-COMPONENT
+np.random.seed(1)
 
-# programmers = ['Alex','Nicole','Sara','Etienne','Chelsea','Jody','Marianne']
+programmers = ['Alex', 'Nicole', 'Sara', 'Etienne', 'Chelsea', 'Jody', 'Marianne']
 
-# base = datetime.datetime.today()
-# dates = base - np.arange(180) * datetime.timedelta(days=1)
-# z = np.random.poisson(size=(len(programmers), len(dates)))
+base = datetime.datetime.today()
+dates = base - np.arange(180) * datetime.timedelta(days=1)
+z = np.random.poisson(size=(len(programmers), len(dates)))
 
-# fig = go.Figure(data=go.Heatmap(
-#         z=z,
-#         x=dates,
-#         y=programmers,
-#         colorscale='Viridis'))
+fig2 = go.Figure(data=go.Heatmap(
+        z=z,
+        x=dates,
+        y=programmers,
+        colorscale='Viridis'))
 
+fig2.update_layout(
+    title='GitHub commits per day',
+    xaxis_nticks=36,
+    height=400)
+# END-HEATMAP-COMPONENT
+data_canada = px.data.gapminder().query("country == 'Canada'")
+fig = px.bar(data_canada, x='year', y='pop', height=400, title="Example Bar Chart")
 # fig.update_layout(
-#     title='GitHub commits per day',
-#     xaxis_nticks=36)
-#END-HEATMAP-COMPONENT
+#
+# )
+
 
 page1 = html.Div(
     [
@@ -34,15 +43,15 @@ page1 = html.Div(
             [
                 dbc.Col(
                     html.Div(
-                        "This is column 1",
-                        style={"height": "400px", "border-style": "solid"},
+                        dcc.Graph(figure=fig),
+                        style={"height": "400px"},
                     ),
                     md=4,
                 ),
                 dbc.Col(
                     html.Div(
-                        "This is column 1",
-                        style={"height": "400px", "border-style": "solid"},
+                        dcc.Graph(figure=fig2),
+                        style={"height": "400px"},
                     ),
                     md=8,
                 ),
@@ -55,18 +64,73 @@ page1 = html.Div(
             [
                 dbc.Col(
                     html.Div(
-                        "This is column 1",
+                        cyto.Cytoscape(
+                            id='cytoscape-compound',
+                            layout={'name': 'preset'},
+                            style={'width': '100%', 'height': '100%'},
+                            stylesheet=[
+                                {
+                                    'selector': 'node',
+                                    'style': {'content': 'data(label)'}
+                                },
+                                {
+                                    'selector': '.countries',
+                                    'style': {'width': 5}
+                                },
+                                {
+                                    'selector': '.cities',
+                                    'style': {'line-style': 'dashed'}
+                                }
+                            ],
+                            elements=[
+                                # Parent Nodes
+                                {
+                                    'data': {'id': 'us', 'label': 'United States'}
+                                },
+                                {
+                                    'data': {'id': 'can', 'label': 'Canada'}
+                                },
+
+                                # Children Nodes
+                                {
+                                    'data': {'id': 'nyc', 'label': 'New York', 'parent': 'us'},
+                                    'position': {'x': 100, 'y': 100}
+                                },
+                                {
+                                    'data': {'id': 'sf', 'label': 'San Francisco', 'parent': 'us'},
+                                    'position': {'x': 100, 'y': 200}
+                                },
+                                {
+                                    'data': {'id': 'mtl', 'label': 'Montreal', 'parent': 'can'},
+                                    'position': {'x': 400, 'y': 100}
+                                },
+
+                                # Edges
+                                {
+                                    'data': {'source': 'can', 'target': 'us'},
+                                    'classes': 'countries'
+                                },
+                                {
+                                    'data': {'source': 'nyc', 'target': 'sf'},
+                                    'classes': 'cities'
+                                },
+                                {
+                                    'data': {'source': 'sf', 'target': 'mtl'},
+                                    'classes': 'cities'
+                                }
+                            ]
+                        ),
                         style={"height": "300px", "border-style": "solid"},
                     ),
-                    md=6,
+                    md=12,
                 ),
-                dbc.Col(
-                    html.Div(
-                        "This is column 2",
-                        style={"height": "300px", "border-style": "solid"},
-                    ),
-                    md=6,
-                ),
+                # dbc.Col(
+                #     html.Div(
+                #         "This is column 2",
+                #         style={"height": "300px", "border-style": "solid"},
+                #     ),
+                #     md=6,
+                # ),
             ],
             no_gutters=True,
         ),
